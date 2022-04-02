2fb5410eaff87cf2942cfdd67bc66f24136f98fd
Kalin Chervenkov <KSChervenkov19@codingburgas.bg>
2022-04-03 00:33:56 +0300

    Add loading

--- App.js
+++ App.js
@@ -1,29 +1,59 @@
-import React, {Component} from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import { WebView } from "react-native-webview";
+import * as Notifications from "expo-notifications";
 
 const App = () => {
-    const editSite = `
-    let selector = document.querySelector(".footer");
+  const [loaded, setLoaded] = useState(false);
 
+  const editSite = `
+    let selector = document.querySelector(".footer");
     let footerText = "<div id='child'>ㅤㅤ© 2020 - Have fun eating :)</div>"
     selector.innerHTML = footerText;
     true;
     `;
-    return(
+
+  return (
+    <>
+      {!loaded && (
+        <SafeAreaView style={styles.loadingTextView}>
+          <Text style={styles.loadingText}>
+            Loading...
+          </Text>
+        </SafeAreaView>
+      )}
+
       <WebView
         source={{ uri: "https://menu.codingburgas.bg" }}
         style={{ marginTop: 20 }}
-        onLoad={() => {
-          return (
-            <View>
-              <Text>Loading...</Text>
-            </View>
-          );
+        onLoadEnd={() => {
+          setLoaded(true);
+        }}
+        onLoadStart={() => {
+          setLoaded(false);
         }}
         injectedJavaScript={editSite}
       />
-    );
+    </>
+  );
 };
+
+const styles = StyleSheet.create({
+  loadingTextView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  loadingText: {
+    textAlignVertical: "center",
+    textAlign: "center",
+    fontSize: 40,
+    fontWeight: "bold",
+    color: "#000000",
+    textTransform: "uppercase",
+    marginTop: "80%",
+  },
+});
 
 export default App;
